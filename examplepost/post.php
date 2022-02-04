c0b49c649329209dd1c28821640cb40610da467e
oussamaa koz <oussamaakalay76@gmail.com>
2022-02-04 07:10:39 -0800

    update

--- examplepost/post.php
+++ examplepost/post.php
@@ -1,9 +1,11 @@
 <?php
     if($_POST['name']  || $_POST['Email'] || $_POST['message']){
         
-        echo "name " . "=" . $_POST['name'] . "<br />";
-        echo "Email " . "=" . $_POST['Email'] . "<br />";
-        echo "youre message " . "=" . $_POST['message'] . "<br />";
+        echo "name " . ":" . $_POST['name'] . "<br />";
+        echo "<br />";
+        echo "Email " . ":" . $_POST['Email'] . "<br />";
+        echo "<br />";
+        echo "youre message " . ":" . $_POST['message'] . "<br />";
     }
 
 ?>
