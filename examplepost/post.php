b17024eb3e0899d6e1087795e68cae6ba9fb1bbd
oussamaa koz <oussamaakalay76@gmail.com>
2022-02-04 04:37:04 -0800

    update

--- examplepost/post.php
+++ examplepost/post.php
@@ -2,7 +2,6 @@
     if($_POST['name'] || $_POST['lastname'] || $_POST['Email'] || $_POST['message']){
         
         echo "name " . "=" . $_POST['name'] . "<br />";
-        echo "lastname " . "=" . $_POST['lastname'] . "<br />";
         echo "Email " . "=" . $_POST['Email'] . "<br />";
         echo "youre message " . "=" . $_POST['message'] . "<br />";
     }
